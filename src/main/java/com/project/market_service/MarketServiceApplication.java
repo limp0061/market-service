57a513bcbc3545cfa1078525936998172a56a653
limp0061 <limp0061@naver.com>
2026-03-26 15:13:03 +0900

    feat: 프로젝트 공통 기반 및 데이터베이스 설정 추가
    - Querydsl 5.0 및 JPA Auditing 설정
    - RedisProperties 및 RedisManager 구현
    - 공통 BaseEntity 추가

--- src/main/java/com/project/market_service/MarketServiceApplication.java
+++ src/main/java/com/project/market_service/MarketServiceApplication.java
@@ -2,12 +2,14 @@ package com.project.market_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class MarketServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarketServiceApplication.class, args);
+    }
 
 }
