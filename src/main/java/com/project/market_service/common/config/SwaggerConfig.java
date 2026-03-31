9251e87d02cafaa66fda39c6c71f834cfc8f6516
limp0061 <limp0061@naver.com>
2026-03-31 15:35:14 +0900

    feat: swagger 공통에러 구현

--- src/main/java/com/project/market_service/common/config/SwaggerConfig.java
+++ src/main/java/com/project/market_service/common/config/SwaggerConfig.java
@@ -8,8 +8,10 @@ import static com.project.market_service.common.constants.AuthConstants.TOKEN_TY
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,5 +42,18 @@ public class SwaggerConfig {
                 .title("Market Service API Document")
                 .description("중고 거래 서비스 API 명세서")
                 .version("1.0.0");
+    }
+
+    @Bean
+    public OperationCustomizer globalApiResponses() {
+        return (operation, handlerMethod) -> {
+            operation.getResponses().addApiResponse("400", new ApiResponse().description("잘못된 요청"));
+            operation.getResponses().addApiResponse("401", new ApiResponse().description("인증 실패"));
+            operation.getResponses().addApiResponse("403", new ApiResponse().description("권한 부족"));
+            operation.getResponses().addApiResponse("404", new ApiResponse().description("존재하지 않는 데이터"));
+            operation.getResponses().addApiResponse("409", new ApiResponse().description("중복된 데이터"));
+            operation.getResponses().addApiResponse("500", new ApiResponse().description("서버 오류"));
+            return operation;
+        };
     }
 }
