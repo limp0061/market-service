a52f4d9ebfc7d42939c403365070dd18f4480cc9
limp0061 <limp0061@naver.com>
2026-04-06 13:56:55 +0900

    chore: mongodb 설정 추가, JpaConfig 멀티DB 설정

--- src/main/java/com/project/market_service/chatroom/exception/ChatRoomErrorCode.java
+++ src/main/java/com/project/market_service/chatroom/exception/ChatRoomErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ChatRoomErrorCode implements ErrorCode {
 
     CANNOT_CHAT_WITH_SELF(HttpStatus.BAD_REQUEST, "자기 자신과 채팅방을 생성할 수 없습니다"),
-    ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 채팅방입니다");
+    ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 채팅방입니다"),
+    CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다");
     private final HttpStatus httpStatus;
     private final String message;
 
--- src/main/java/com/project/market_service/common/config/JpaConfig.java
+++ src/main/java/com/project/market_service/common/config/JpaConfig.java
@@ -4,11 +4,22 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+        basePackages = "com.project.market_service",
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = MongoRepository.class
+        )
+)
 @RequiredArgsConstructor
 public class JpaConfig {
 
